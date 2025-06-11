c16f970f5ed017471ef3c84372b8be04ebd48d11
DevDuulat <duulat.bekm@mail.ru>
2025-06-12 01:32:11 +0600

    created layout TikhonovIvan  Co-authored-by: TikhonovIvan <TikhonovIvan@users.noreply.github.com>

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -23,7 +23,6 @@
       </div>
       <div class="col-lg-3">
         <p>
-          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
           Copyright &copy;
           <script>
             document.write(new Date().getFullYear());
