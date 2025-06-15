71403984669e73886c1873308f604bd65f4b0b6a
DevDuulat <duulat.bekm@mail.ru>
2025-06-15 11:12:15 +0600

    Co-authored-by: ivan <ivntik77@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
   <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&display=swap"
     rel="stylesheet" />
-
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" type="text/css" />
   <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}" type="text/css" />
   <link rel="stylesheet" href="{{ asset('css/elegant-icons.css') }}" type="text/css" />
